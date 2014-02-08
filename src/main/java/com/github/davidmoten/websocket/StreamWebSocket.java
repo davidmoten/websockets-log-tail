609e89876657faaae793afee8bb023d7109021ae
Dave Moten <davidmoten@gmail.com>
2014-02-08 12:25:56 +1100

    initial commit

---
+++ src/main/java/com/github/davidmoten/websocket/StreamWebSocket.java
@@ -0,0 +1,47 @@
+package com.github.davidmoten.websocket;
+
+import java.io.IOException;
+
+import org.eclipse.jetty.websocket.WebSocket;
+
+import rx.Observable;
+import rx.Subscription;
+import rx.util.functions.Action1;
+
+public class StreamWebSocket implements WebSocket {
+
+	private final Observable<?> stream;
+	private Connection connection;
+
+	private Subscription subscription;
+
+	public StreamWebSocket(Observable<?> stream) {
+		this.stream = stream;
+	}
+
+	public void onMessage(String message) {
+		try {
+			System.out.println("sending " + message);
+			connection.sendMessage(message);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	@Override
+	public void onClose(int closeCode, String message) {
+		subscription.unsubscribe();
+	}
+
+	@Override
+	public void onOpen(Connection connection) {
+		this.connection = connection;
+		this.subscription = stream.subscribe(new Action1<Object>() {
+			@Override
+			public void call(Object obj) {
+				onMessage(obj.toString());
+			}
+		});
+	}
+
+}
---
+++ src/main/java/com/github/davidmoten/websocket/ViewerServlet.java
@@ -0,0 +1,50 @@
+package com.github.davidmoten.websocket;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.eclipse.jetty.websocket.WebSocket;
+import org.eclipse.jetty.websocket.WebSocketServlet;
+
+import rx.Observable;
+import rx.Observable.OnSubscribeFunc;
+import rx.Observer;
+import rx.Subscription;
+import rx.util.Range;
+import rx.util.functions.Func1;
+
+public class ViewerServlet extends WebSocketServlet {
+	private static final long serialVersionUID = -1151103043909699131L;
+
+	@Override
+	public WebSocket doWebSocketConnect(HttpServletRequest req, String protocol) {
+		Observable<?> stream = createObservable();
+		return new StreamWebSocket(stream);
+	}
+
+	private Observable<String> createObservable() {
+		return Observable.create(new OnSubscribeFunc<Integer>() {
+
+			@Override
+			public Subscription onSubscribe(Observer<? super Integer> o) {
+				final RangeRunnable runnable = new RangeRunnable(Range.create(
+						1, 100000), o);
+				Thread t = new Thread(runnable);
+				t.start();
+				return new Subscription() {
+
+					@Override
+					public void unsubscribe() {
+						runnable.cancel();
+					}
+				};
+			}
+		}).map(new Func1<Integer, String>() {
+
+			@Override
+			public String call(Integer i) {
+				return i
+						+ " this a longish log message similar in length to many typical log lines";
+			}
+		});
+	}
+}
