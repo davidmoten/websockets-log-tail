cf5c3b7b6a6783fff75d7b9b2a1ecfe6454d085d
Dave Moten <davidmoten@gmail.com>
2014-02-10 13:16:31 +1100

    simplify

--- src/main/java/com/github/davidmoten/websocket/ViewerServlet.java
+++ src/main/java/com/github/davidmoten/websocket/ViewerServlet.java
@@ -5,15 +5,11 @@ import javax.servlet.http.HttpServletRequest;
 import org.eclipse.jetty.websocket.WebSocket;
 import org.eclipse.jetty.websocket.WebSocketServlet;
 
-import rx.Observable;
-
 public class ViewerServlet extends WebSocketServlet {
 	private static final long serialVersionUID = -1151103043909699131L;
 
 	@Override
 	public WebSocket doWebSocketConnect(HttpServletRequest req, String protocol) {
-		Observable<?> stream = Util.createDummyStream();
-		return new StreamWebSocket(stream);
+		return new StreamWebSocket(Util.createDummyStream());
 	}
-
 }
