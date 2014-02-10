f40b9cc457a9a21ad2d27ba249b8aced722004a1
Dave Moten <davidmoten@gmail.com>
2014-02-10 11:19:03 +1100

    simplify

---
+++ src/main/java/com/github/davidmoten/websocket/Util.java
@@ -0,0 +1,25 @@
+package com.github.davidmoten.websocket;
+
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.util.functions.Func1;
+
+class Util {
+
+	static Observable<String> createDummyStream() {
+		return Observable.interval(100, TimeUnit.MILLISECONDS).map(toMessage());
+	}
+
+	static Func1<Long, String> toMessage() {
+		return new Func1<Long, String>() {
+
+			@Override
+			public String call(Long i) {
+				return i
+						+ " this a longish log message similar in length to many typical log lines";
+			}
+		};
+	}
+
+}
--- src/main/java/com/github/davidmoten/websocket/ViewerServlet.java
+++ src/main/java/com/github/davidmoten/websocket/ViewerServlet.java
@@ -1,36 +1,19 @@
 package com.github.davidmoten.websocket;
 
-import java.util.concurrent.TimeUnit;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.eclipse.jetty.websocket.WebSocket;
 import org.eclipse.jetty.websocket.WebSocketServlet;
 
 import rx.Observable;
-import rx.util.functions.Func1;
 
 public class ViewerServlet extends WebSocketServlet {
 	private static final long serialVersionUID = -1151103043909699131L;
 
 	@Override
 	public WebSocket doWebSocketConnect(HttpServletRequest req, String protocol) {
-		Observable<?> stream = createDummyStream();
+		Observable<?> stream = Util.createDummyStream();
 		return new StreamWebSocket(stream);
 	}
 
-	private Observable<String> createDummyStream() {
-		return Observable.interval(100, TimeUnit.MILLISECONDS).map(toMessage());
-	}
-
-	private Func1<Long, String> toMessage() {
-		return new Func1<Long, String>() {
-
-			@Override
-			public String call(Long i) {
-				return i
-						+ " this a longish log message similar in length to many typical log lines";
-			}
-		};
-	}
 }
